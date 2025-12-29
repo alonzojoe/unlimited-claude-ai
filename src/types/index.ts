b8a746d39e511995a81c9f6c4a6a18d2d96dc5fe
alonzojoe <alonzojoenell2k15@gmail.com>
2025-12-29 09:45:04 +0800

    create new types for Chat and Message.

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface Message {
+    id: string;
+    role: 'user' | 'assistant',
+    content: string;
+}
+
+export interface Chat {
+    id: string;
+    title: string;
+    preview: string;
+    message: Message[]
+}
+
